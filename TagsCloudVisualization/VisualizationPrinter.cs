eae5a74a0880566ea17b1c4ad7254b9632bc0376
dm7672 <dmitrsem10@gmail.com>
2025-11-30 02:17:20 +0500

    Визуализатор

---
+++ TagsCloudVisualization/VisualizationPrinter.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.IO;
+using System.Linq;
+
+namespace TagCloud
+{
+    public static class VisualizationPrinter
+    {
+        public static string PrintVisualization(string name, IEnumerable<Rectangle> rectangles, Point center)
+        {
+
+        }
+
+        private static string GetPath(string name)
+        {
+
+        }
+
+        private static Rectangle GetBounds(IEnumerable<Rectangle> rects)
+        {
+
+        }
+    }
+}
