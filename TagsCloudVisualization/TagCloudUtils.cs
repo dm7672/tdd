2c1bd9923f4b030f921c08b8806b729727c89ba0
dm7672 <dmitrsem10@gmail.com>
2025-11-30 02:14:19 +0500

    статические вспомогательные методы TagCloudUtils

---
+++ TagsCloudVisualization/TagCloudUtils.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+
+
+namespace TagCloud
+{
+    internal static class TagCloudUtils
+    {
+        public static double Distance(Point a, Point b)
+        {
+
+        }
+
+
+        public static double DistanceToCenter(Rectangle rect, Point center)
+        {
+
+        }
+
+
+        public static bool IntersectsAny(Rectangle rect, IEnumerable<Rectangle> others)
+        {
+
+        }
+
+
+        public static Rectangle MoveHorizontalTowardsCenter(Rectangle rect, Point center)
+        {
+
+        }
+        public static Rectangle MoveVerticalTowardsCenter(Rectangle rect, Point center)
+        {
+
+        }
+    }
+}
