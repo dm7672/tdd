6a819b67471c9423c854d726ce5ab36ad6ecdf18
dm7672 <dmitrsem10@gmail.com>
2025-11-30 02:12:44 +0500

    Основной класс CircularCloudLayouter

---
+++ TagsCloudVisualization/CircularCloudLayouter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+
+namespace TagCloud
+{
+    public class CircularCloudLayouter
+    {
+        private Point Center { get; }
+        private readonly List<Rectangle> rectangles = new List<Rectangle>();
+        public IReadOnlyList<Rectangle> Rectangles => rectangles.AsReadOnly();
+
+        private readonly IPointGetter pointGetter;
+
+        public CircularCloudLayouter(Point center, IPointGetter pointGetter)
+        {
+            this.Center = center;
+            this.pointGetter = pointGetter;
+        }
+
+        public Rectangle PutNextRectangle(Size rectangleSize)
+        {
+
+        }
+
+        private Rectangle MoveCloserToCenter(Rectangle rect)
+        {
+
+        }
+    }
+}
