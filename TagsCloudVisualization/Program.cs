939d673ae8c11659d70ae43ad7f8eec46bd987b8
dm7672 <dmitrsem10@gmail.com>
2025-12-02 23:55:14 +0500

    Визуализация некоторых примеров

---
+++ TagsCloudVisualization/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+namespace TagCloud
+{
+    class Program
+    {
+        static void Main()
+        {
+            var center = new Point(600, 400);
+            var rnd = new Random(0);
+            var layouter = new CircularCloudLayouter(center, new Spiral(center));
+            for (int k = 0; k < 3; k++)
+            {
+                layouter = new CircularCloudLayouter(center, new Spiral(center));
+                for (int i = 0; i < 200; i++)
+                {
+                    var size = new Size(rnd.Next(8 + k * 2, 60 + k * 100), rnd.Next(6 + k, 40 + k * 100));
+                    layouter.PutNextRectangle(size);
+                }
+                VisualizationPrinter.Print($"Случайные прямоугольники {k}", layouter.Rectangles, center);
+            }
+            layouter = new CircularCloudLayouter(center, new Spiral(center));
+            for (int i = 0; i < 100; i++)
+            {
+                var size = new Size(100, 100);
+                layouter.PutNextRectangle(size);
+            }
+            VisualizationPrinter.Print("Квадраты", layouter.Rectangles, center);
+        }
+    }
+}
---
+++ TagsCloudVisualization/README.md
@@ -0,0 +1,4 @@
+![Квадраты](visualizations/visualization-Квадраты.png)
+![Маленькие прямоугольники](visualizations/visualization-Случайные прямоугольники 0.png)
+![Средние прямоугольники](visualizations/visualization-Случайные прямоугольники 1.png)
+![Большие прямоугольники](visualizations/visualization-Случайные прямоугольники 2.png)
